51ec849c5252edffdfcb1ab4a9b384e9573f4a0a
Cheney Ni <56236030+cheneyni-451@users.noreply.github.com>
2023-12-10 16:25:47 -0500

    Add skeleton layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<nav>
+	<div class="nav-left">
+		<ul>
+			<li><a href="/">Home</a></li>
+			<li><a href="/projects">Projects</a></li>
+			<li><a href="/resume">Resume</a></li>
+		</ul>
+	</div>
+	<div class="nav-right">
+		<ul>
+			<li><a href="https://github.com/cheneyni-451" target="_blank">GitHub</a></li>
+			<li><a href="https://www.linkedin.com/in/cheneyni" target="_blank">LinkedIn</a></li>
+		</ul>
+	</div>
+</nav>
+
+<slot />
