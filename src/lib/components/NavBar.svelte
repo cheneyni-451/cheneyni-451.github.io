2ea8b858dc9a0328c39d0634da3338336552fc0f
Cheney Ni <56236030+cheneyni-451@users.noreply.github.com>
2023-12-22 12:37:48 -0500

    Add icon transitions

--- src/lib/components/NavBar.svelte
+++ src/lib/components/NavBar.svelte
@@ -4,19 +4,30 @@
 	import { onMount } from 'svelte';
 	import { flyblur } from '$lib/transitions';
 	import { slide } from 'svelte/transition';
+	import { tweened } from 'svelte/motion';
+	import { expoOut } from 'svelte/easing';
 
 	let ready = false;
 	onMount(() => (ready = true));
 
 	const linksLeft = [
-		{ text: 'Home', url: '/' },
+		{ text: 'Cheney Ni', url: '/' },
 		{ text: 'Projects', url: '/projects' },
 		{ text: 'Resume', url: '/resume' }
 	];
 	const linksRight = [
-		{ iconComponent: GitHubIcon, url: 'https://github.com/cheneyni-451' },
-		{ iconComponent: LinkedInIcon, url: 'https://www.linkedin.com/in/cheneyni' }
+		{
+			iconComponent: GitHubIcon,
+			url: 'https://github.com/cheneyni-451',
+			scale: tweened(1, { duration: 500, easing: expoOut })
+		},
+		{
+			iconComponent: LinkedInIcon,
+			url: 'https://www.linkedin.com/in/cheneyni',
+			scale: tweened(1, { duration: 500, easing: expoOut })
+		}
 	];
+
 	let navDivTransitioned = false;
 	let navLeftTransitioned = false;
 	let navRightTransitioned = false;
@@ -25,14 +36,14 @@
 
 {#if ready}
 	<nav
-		id="navbar"
+		class="navbar"
 		in:flyblur={{ duration: 1000, y: -100 }}
 		on:introend={() => {
 			navDivTransitioned = true;
 			navBarTransitioned = navDivTransitioned && navLeftTransitioned && navRightTransitioned;
 		}}
 	>
-		<ul id="navbar-left">
+		<ul class="navbar-left">
 			{#each linksLeft as { url, text }, i}
 				<li
 					in:slide|global={{ delay: i * 200 + 400, duration: 200 }}
@@ -44,12 +55,12 @@
 						}
 					}}
 				>
-					<a href={url}>{text}</a>
+					<a class="site-links" href={url}>{text}</a>
 				</li>
 			{/each}
 		</ul>
-		<ul id="navbar-right">
-			{#each linksRight as { url, iconComponent }, i}
+		<ul class="navbar-right">
+			{#each linksRight as { url, iconComponent, scale }, i}
 				<li
 					in:slide|global={{
 						delay: (linksRight.length - 1 - i) * 400 + 400,
@@ -63,7 +74,13 @@
 						}
 					}}
 				>
-					<a href={url} target="_blank"><svelte:component this={iconComponent} /></a>
+					<a
+						on:pointerenter={() => scale.set(1.6)}
+						on:pointerleave={() => scale.set(1)}
+						class="social-links"
+						href={url}
+						target="_blank"><svelte:component this={iconComponent} {scale} /></a
+					>
 				</li>
 			{/each}
 		</ul>
@@ -74,7 +91,7 @@
 	@import '$lib/styles/colors.css';
 	@import '$lib/styles/main.css';
 
-	#navbar {
+	.navbar {
 		display: flex;
 		flex-direction: row;
 		justify-content: space-between;
@@ -84,17 +101,49 @@
 		backdrop-filter: blur(10px);
 	}
 
-	#navbar-left,
-	#navbar-right {
+	.navbar-left,
+	.navbar-right {
 		display: flex;
 		flex-direction: row;
+		align-items: baseline;
 		list-style-type: none;
 		gap: 2em;
 		padding: 0;
 	}
 
-	#navbar-left a,
-	#navbar-right a {
+	.site-links,
+	.social-links {
 		text-decoration: none;
+		position: relative;
+	}
+
+	.site-links::before {
+		content: '';
+		position: absolute;
+		display: block;
+		width: 100%;
+		height: 2px;
+		bottom: 0;
+		left: 0;
+		background-color: var(--cp-mocha-sky);
+		transform: scaleX(0);
+		transition: transform 0.3s ease;
+	}
+
+	.site-links:hover::before {
+		transform: scaleX(1);
+	}
+
+	.social-links {
+		fill: var(--cp-mocha-text);
+	}
+
+	.social-links:hover {
+		fill: var(--cp-mocha-sky);
+	}
+
+	.navbar-left li:first-child {
+		font-size: larger;
+		font-weight: bold;
 	}
 </style>
